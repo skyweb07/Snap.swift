c17299951ffc791fcc507fa4ac59d7d809c201d8
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2017-11-23 12:11:49 +0100

    Initial Commit

---
+++ Snap/Snap.h
@@ -0,0 +1,19 @@
+//
+//  Snap.h
+//  Snap
+//
+//  Created by skyweb07 on 23/11/2017.
+//  Copyright © 2017 Skydev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Snap.
+FOUNDATION_EXPORT double SnapVersionNumber;
+
+//! Project version string for Snap.
+FOUNDATION_EXPORT const unsigned char SnapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snap/PublicHeader.h>
+
+
