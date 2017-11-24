5b49c6b9d5b25dce0f1917770ed71bd883501a55
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-24 17:45:52 +0100

    Separate `Recordable` protocol and `XCTestCase` extension

--- Snap/Core/Application/Recordable/Recordable.swift
+++ Snap/Core/Application/Recordable/Recordable.swift
@@ -1,21 +1,6 @@
 import XCTest
-import ObjectiveC
 
 public protocol Recordable {
   var isRecording: Bool { get set }
 }
 
-// MARK: - XCTestcase + Recordable
-
-private var recordingPointer: UInt8 = 0
-
-extension XCTestCase: Recordable {
-  public var isRecording: Bool {
-    get {
-      return (objc_getAssociatedObject(self, &recordingPointer) as? Bool) ?? false
-    }
-    set {
-      objc_setAssociatedObject(self, &recordingPointer, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-  }
-}
