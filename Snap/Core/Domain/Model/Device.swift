e51fc87ce77d16f95a775a8855b4266f8ea160f3
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-12-18 13:25:35 +0100

    Add device domain model containing the list of possible matchers

---
+++ Snap/Core/Domain/Model/Device.swift
@@ -0,0 +1,11 @@
+public enum Device {
+  case iPhone_SE
+  case iPhone_5         // iPhone 5, 5C, 5S, iPod Touch 5g
+  case iPhone_8         // iPhone 6, iPhone 6s, iPhone 7
+  case iPhone_8_Plus    // iPhone 6 Plus, iPhone 6s Plus, iPhone 7 Plus
+  case iPhone_X
+  case iPad_Mini        // iPad Mini 2, iPad Mini 3, iPad Mini 4
+  case iPad_Air         // iPad 3, iPad 4, iPad Air, iPad Air 2, 9.7-inch iPad Pro
+  case iPad_Pro_10_5
+  case ipad_Pro_12_9    // 12.9-inch iPad Pro
+}
