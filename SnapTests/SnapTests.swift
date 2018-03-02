d0566a1bc79e694a997c9e07af08b595062b02ba
Ralf Ebert <info@ralfebert.de>
2018-03-02 17:18:20 +0100

    fix layer test case to generate the gradient that is visible in the test image

--- SnapTests/SnapTests.swift
+++ SnapTests/SnapTests.swift
@@ -34,8 +34,8 @@ private final class SnapTests: XCTestCase {
       UIColor.white.cgColor
     ]
     layer.locations = [0.0, 1.0]
-    layer.startPoint = CGPoint(x: 0.0, y: 1.0)
-    layer.endPoint = CGPoint(x: 0.0, y: 1.0)
+    layer.startPoint = CGPoint(x: 0.0, y: 0.0)
+    layer.endPoint = CGPoint(x: 1.0, y: 0.0)
 
     expect(layer).toMatchSnapshot()
   }
